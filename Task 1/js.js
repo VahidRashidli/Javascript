1628be532f7fc50577142fbea06d3e6594b29580
VahidRashidli <88052187+VahidRashidli@users.noreply.github.com>
2021-10-21 13:26:09 +0400

    Delete Task 1 directory

--- Task 1/js.js
+++
@@ -1,9 +0,0 @@
-let length = parseInt(prompt("Yolkamizin uzunlugu:"));
-let star = "*";
-for (let i = 0; i < length; i++) {
-    console.log(`                                                                                     ${star}                              `);
-    star = star.split("");
-    star.push("*");
-    star.unshift("*");
-    star = star.join("");
-}
